914322ce9eda6ffd85f9cd46ed09ecdf3db30031
unknown <220477019@mycput.ac.za>
2023-05-05 18:14:16 +0200

    Added some comments

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -5,16 +5,15 @@ import java.util.List;
 
 public class Calculator
 {
-
     double calculateFV(double presentV, double intRate, int y)
     {
       double pv = presentV;
       double ir = intRate;
-      double convertedRate = intRate/100;
+      double convertedRate = intRate/100; //Convert interest rate from percentage to decimal
       int years = y;
       double result;
 
-      result = pv * Math.pow((1+convertedRate), years);
+      result = pv * Math.pow((1+convertedRate), years); //Formal equation FV = PV(1+r)^n) where n=years; r= interest rate
       return result;
     }
 
@@ -27,17 +26,13 @@ public class Calculator
             double fvCalculated = calculateFV(presentV, intRate, i+1);
             FututeValue fv = new FututeValue(i+1, presentV,fvCalculated,intRate);
             list.add(fv);
-
-            //System.out.println("Year: " + (i+1) + ", Future Value: " + fv.getFutureValue());
-
         }
         return  list;
-
     }
 
     public static void main(String[] args) {
         Calculator cltr = new Calculator();
-        List<FututeValue> lst = cltr.fvList(23000, 6.4, 100);
+        List<FututeValue> lst = cltr.fvList(23000, 6.4, 3);
 
         for(FututeValue fv : lst)
         {
